6ba4d1fe95ae57af9003bc2da3d8e1a42396c19a
piaacv <122986202+piaacv@users.noreply.github.com>
2024-03-07 20:45:52 -0300

    Proyecto terminado según requerimientos HTML, CSS y Boostrap JS

--- Downloads/fullstack/m2-s4-d5-viajes_chile/assets/js/script.js
+++ Downloads/fullstack/m2-s4-d5-viajes_chile/assets/js/script.js
@@ -1,2 +1,14 @@
+// Tooltip Bootstrap
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Toast Bootstrap
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+        toastBootstrap.show()
+    })
+}
